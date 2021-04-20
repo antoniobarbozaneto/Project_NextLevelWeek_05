407de4f2880743689b1d0bf1e8c25cd97749c45c
Antonio Barboza Neto <neto.barboza1995@gmail.com>
2021-04-20 01:13:34 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação especifica
+ */
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Olá NLW #05!",
+  });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Usuário salvo com sucesso!" });
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
